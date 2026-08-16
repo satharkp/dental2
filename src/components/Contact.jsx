75d601dc235754d5e3fd858a30f2d6442b7c4ea1
Abdul Sathar <satharkp292@gmail.com>
2026-08-16 11:50:11 +0530

    map added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -161,6 +161,19 @@ const Contact = () => {
           </div>
 
         </div>
+        {/* Google Maps Location Embed */}
+        <div className="max-w-5xl mx-auto mt-12 rounded-2xl overflow-hidden shadow-lg border border-slate-200">
+          <iframe 
+            title="Clinic Location Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.4357778550186!2d75.7958933!3d11.2294156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6593450942475%3A0xc3c5f4b505963953!2sNaduvattam%2C%20Kozhikode%2C%20Kerala!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
+            width="100%" 
+            height="320" 
+            style={{ border: 0 }} 
+            allowFullScreen="" 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
 
       </div>
     </section>
